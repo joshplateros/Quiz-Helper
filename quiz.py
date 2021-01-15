8624513a01d257f0a865f68df4d0298900dbbc30
Joshua <josh.plateros13@gmail.com>
2021-01-14 16:56:49 -0800

    Initial screen

--- quiz.py
+++ quiz.py
@@ -11,34 +11,81 @@ class UI(QWidget):
         
         # Pages
         self.menuSelection = QWidget()
+        self.classesSelection = QWidget()
 
         # Call pages, this is first page
         self.beginningMenu()
+        self.classesMenu()
         # This is second page
         # INSERT 
 
         self.menu.addWidget(self.menuSelection)
+        self.menu.addWidget(self.classesSelection)
 
     def beginningMenu(self):
         self.menuSelection.resize(550, 400)
         
-        self.mainLayout = QVBoxLayout()
-        self.menuButtonLayout = QHBoxLayout()
+        mainLayout = QVBoxLayout()
+        menuButtonLayout = QHBoxLayout()
 
         # Main menu title
-        self.mainMenuTitle = QLabel(self.menuSelection)
-        self.mainMenuTitle.setGeometry(QRect(130, -30, 300, 200))
-        self.mainMenuTitle.setAlignment(Qt.AlignCenter)
-        self.mainMenuTitle.setText("Quiz!")
-        self.mainMenuTitle.setStyleSheet("font: 14pt Century Gothic")
+        mainMenuTitle = QLabel(self.menuSelection)
+        mainMenuTitle.setGeometry(QRect(130, -30, 300, 200))
+        mainMenuTitle.setAlignment(Qt.AlignCenter)
+        mainMenuTitle.setText("Quiz!")
+        mainMenuTitle.setStyleSheet("font: 14pt Century Gothic")
         self.menuSelection.setWindowTitle("Quiz!!")
 
         self.classesSettings = QPushButton('Classes', self)
-        self.menuButtonLayout.addWidget(self.classesSettings)
+        menuButtonLayout.addWidget(self.classesSettings)
 
         
-        self.mainLayout.addLayout(self.menuButtonLayout) 
-        self.menuSelection.setLayout(self.mainLayout)
+        mainLayout.addLayout(menuButtonLayout) 
+        self.menuSelection.setLayout(mainLayout)
+
+    def classesMenu(self):
+        self.classesSelection.resize(550, 400)
+
+        classMainLayout = QVBoxLayout()
+        classButtonLayout = QHBoxLayout()
+
+        classEditButtonLayout = QHBoxLayout()
+
+        classMenuTitle = QLabel(self.classesSelection)
+        classMenuTitle.setAlignment(Qt.AlignCenter)
+        classMenuTitle.setText("Classes")
+        classMenuTitle.setStyleSheet("font: 14pt Century Gothic")
+        classMenuTitle.adjustSize()
+        classMenuTitle.move(50, 100)
+        self.classesSelection.setWindowTitle("Quiz!!")
+
+        # Edit classes
+        classRemoveButton = QPushButton("Remove class")
+        classRemoveButton.resize(100, 20)
+        classAddButton = QPushButton("Add class")
+        classEditButtonLayout.addWidget(classRemoveButton)
+        classEditButtonLayout.addWidget(classAddButton)
+
+        # Setting the classes
+        classList = QListWidget()
+        currentClasses = getClasses()
+        for i in currentClasses:
+            classList.insertItem(0, i)
+
+    
+        # Continue buttons, back
+        classButtonLayout.addWidget(self.menuButton, alignment=Qt.AlignRight)
+        classContinue = QPushButton("Continue", self)
+        classButtonLayout.addWidget(classContinue)
+
+        classMainLayout.addLayout(classButtonLayout)
+        classButtonLayout.addWidget(classList)
+        classMainLayout.addLayout(classEditButtonLayout)
+        self.classesSelection.setLayout(classMainLayout)
+
+
+
+
 
 
 class Controller(QMainWindow, UI):
@@ -47,9 +94,21 @@ class Controller(QMainWindow, UI):
         self.setup(self)
 
         self.menuButton.clicked.connect(self.menuWindow)
+        self.classesSettings.clicked.connect(self.classesMenuSelection)
 
     def menuWindow(self):
         self.menu.setCurrentIndex(0)
+
+    def classesMenuSelection(self):
+        self.menu.setCurrentIndex(1)
+
+# Read in classes
+def getClasses():
+    classes = []
+    classes.append("Class 1")
+    classes.append("Class 2")
+
+    return classes
 
 
 def window():
